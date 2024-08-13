b33fe7709e0a8450d8bde60bae74220d250f52ec
HAHA-1314 <qq1879009491@hotmail.com>
2024-08-13 13:20:19 +0800

    Revert "全局页面完善"
    This reverts commit 79e299bb011f456791ad4bf736a049f9c1acc199.

--- src/views/user-system.vue
+++ src/views/user-system.vue
@@ -1,95 +1,126 @@
 <template>
-
-
-<!-- 组别成员页面 -->
-<div class="bigBox">
-  
-  <!-- 侧边导航栏 组别成员无账号管理选项 -->
-<div class="nav">
-    <el-col >
-      <div class="header"> <span class="circle">
-         <el-icon ><Monitor /></el-icon>
-      </span>     
-      <h5 class="nav-header">运营组</h5></div>
-     
-      <el-menu
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-      >  <el-menu-item >
-        
-           <span><el-icon color="blue"><HomeFilled /></el-icon>首页</span>
-        </el-menu-item>
-        <el-sub-menu >
-          <template #title>
-           
-            <span><el-icon><User /></el-icon>考核管理
-            </span>
-          </template>
-          <el-menu-item class="menu-item">人员管理</el-menu-item>
-            <el-menu-item class="menu-item">考核管理</el-menu-item>        
-            <el-menu-item class="menu-item">预约管理</el-menu-item>
-            <el-menu-item class="menu-item">公告设置</el-menu-item>
-          
-        </el-sub-menu>
-        <el-sub-menu >
-          <template #title>
-           
-            <span > <el-icon><Message /></el-icon>信息管理</span>
-          </template>
-        <el-menu-item class="menu-item">团队介绍</el-menu-item>
-            <el-menu-item class="menu-item">组别介绍</el-menu-item>      
-            <el-menu-item class="menu-item">项目介绍</el-menu-item>
-            <el-menu-item class="menu-item">精选推文</el-menu-item>
-        </el-sub-menu>
-   
-       
-      </el-menu>
-    </el-col>
-</div>
-
-
-<!-- 右上不变的全局 -->
-<div class="content">
-
-  <div class="blue"></div>
-
-  <!-- 右边横向tab栏 -->
-  <div class="sub-nav">
-    <div class="nav-left"></div>
-    <span><el-icon><ArrowLeftBold /></el-icon></span>
-    <span>首页</span>
-    <span>团队介绍</span>
-    <div class="nav-right">
-      <span><el-icon><ArrowRightBold /></el-icon></span>
-      <span class="update-box"><el-icon class="update"><Refresh /></el-icon>  刷新</span>
-    </div>
+  <div class="common-layout">
+    <!-- ElementUI 预设布局 -->
+    <el-container>
+      <el-aside width="20%">
+        <!-- 侧边==账号名字 -->
+        <div class="header">
+          <span class="circle">
+            <el-icon><Monitor /></el-icon>
+          </span>
+          <h5 class="nav-header">运营组</h5>
+        </div>
+        <!-- 侧边==导航栏 -->
+        <el-menu
+          default-active="1"
+          @open="handleMenuOpen"
+          @close="handleClose"
+          style="user-select: none"
+        >
+          <!-- (index) 首页 1  || 考核管理 2 -> 人员管理 2-1 | 考核管理 2-2 | 预约管理 2-3 | 公告设置 2-4 || 信息管理 3 -> 团队管理 3-1 | 组别管理 3-2 | 项目介绍 3-3 | 精选推文 3-4 || -->
+          <el-menu-item index="1">
+            <el-icon><House /></el-icon>
+            <span>首页</span>
+          </el-menu-item>
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon><User /></el-icon>
+              <span>考核管理</span>
+            </template>
+            <el-menu-item index="2-1">人员管理</el-menu-item>
+            <el-menu-item index="2-2">考核管理</el-menu-item>
+            <el-menu-item index="2-3">预约管理</el-menu-item>
+            <el-menu-item index="2-4">公告设置</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="3">
+            <template #title>
+              <el-icon><Message /></el-icon>
+              <span>信息管理</span>
+            </template>
+            <el-menu-item index="3-1">团队介绍</el-menu-item>
+            <el-menu-item index="3-2">组别介绍</el-menu-item>
+            <el-menu-item index="3-3">项目介绍</el-menu-item>
+            <el-menu-item index="3-4">精选推文</el-menu-item>
+          </el-sub-menu>
+         
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <!-- 主体==头部==蓝色条块 以及 可增减标签页 -->
+        <el-header height="85px" width="80%" class="tabs-box">
+          <div class="blue"> <el-button type="primary" class="sign-out" >退出登录</el-button></div>
+          <el-tabs
+            v-model="editableTabsValue"
+            type="card"
+            class="demo-tabs"
+            closable
+            @tab-click="clickTab"
+            @tab-remove="removeTab"
+          >
+            <el-tab-pane
+              v-for="item in editableTabs"
+              :key="item.name"
+              :label="item.title"
+              :name="item.name"
+            >
+              {{ item.content }}
+            </el-tab-pane>
+          </el-tabs>
+        </el-header>
+        <el-main>
+          <!-- 主题==重要内容 -->
+          <span>main内容</span>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
-</div></div>
 </template>
 
-
-<script >
-
-
-
+<script>
+export default {
+  data() {
+    return {
+      tabIndex: 1,
+      editableTabsValue: "1",
+      editableTabs: [
+        {
+          title: "首页",
+          name: "1",
+          content: "",
+        },
+        {
+          title: "test",
+          name: "2",
+          content: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleMenuOpen(key, keyPath) {
+      //回调函数 -- 点击菜单
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      //回调函数 -- 关闭菜单
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
-
-
 
 <style lang="css" scoped>
 
-.sub-nav span{
-  cursor: pointer;
+.common-layout{
+margin: -8px;
 }
 
-
-.bigBox{
-  display: flex;
-  
+.tabs-box{
+  padding: 0;
 }
 
-.circle{
+.circle {
   height: 30px;
   width: 30px;
   border-radius: 50cqb;
@@ -100,89 +131,33 @@
   color: white;
 }
 
-.header{
+.header {
   margin-left: 20px;
   width: 100px;
   height: 60px;
   display: flex;
   align-items: center;
-  justify-content:space-between;
-  
+  justify-content: space-between;
 }
 
-.nav-header{
-  font-size: 22px;
-  color:#006eff;
+.nav-header {
+  font-size: 18px;
+  font-weight: 400;
+  color: #006eff;
 }
 
-span{
-  width: 300px;
-   font-size: 15px;
-  color: #778187;
-}
-span:hover {
- color:#006eff;
-}
-
-.menu-item:hover{
- color:#006eff;
-
-}
-
-.menu-item{
-  font-size: 15px;
-  color: #778187;
-  background-color: #f7f7f7
-}
-.nav{
-  width: 20%;
-}
-
-.content{
-width: 80%;
-height: 500px;
-background-color: #fff;
-}
-
-.blue{
+.blue {
+  position: relative;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 60px;
   background-color: #006eff;
 }
 
-.sub-nav{
-   margin-top: 2px;
-  width: 100%;
-  height: 50px;
-  background-color: #fff;
-  border-bottom: 1px solid #e4e4e4;
+.sign-out{
+  position: absolute;
+  right: 15px;
+  top: 12px;
 }
-
-.nav-left{
-  float: left;
-}
-
-.nav-right{
-  float:right
-}
-
-.sub-nav span{
-
-  font-size: 17px;
-  padding:15px;
-  height: 50px;
-  width: 50px;
-  line-height: 50px;
- /* display: inline-block; */
-  border-left: 0;
-  border-right:1px solid #e4e4e4;
-}
-
-
-.update{
-padding-right: 10px;
-font-size: 17px;
-
-}
-
 </style>
